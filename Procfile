eb719257fe83d274180ec842126c4f69cbf4a9ca
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2021-03-13 09:57:59 +0530

    Revive

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 sh1vamrun.py
---
+++ sh1vamrun.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/Sh1vam/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install -r requirements.txt && python3 -m userbot")
