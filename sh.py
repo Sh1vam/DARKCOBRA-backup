47cd491f4f944cb5495a6296fdab95a7fa55a32e
Danish <72792730+1Danish-00@users.noreply.github.com>
2021-03-13 11:39:25 +0530

    Update sh.py

--- sh.py
+++ sh.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/Sh1vam/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install -r requirements.txt && python3 -m userbot")
+os.system("git clone https://github.com/1Danish-00/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install -r requirements.txt && python3 -m userbot")
