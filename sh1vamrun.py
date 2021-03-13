0f92102b616a269de4befbe71dae89a364144687
Danish <72792730+1Danish-00@users.noreply.github.com>
2021-03-13 10:26:27 +0530

    Update sh1vamrun.py

--- sh1vamrun.py
+++ sh1vamrun.py
@@ -1,13 +1,5 @@
-import asyncio
+import os
 try:
-    cmd = ["git clone https://github.com/Sh1vam/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install -r requirements.txt && python3 -m userbot"]
-    process = await asyncio.create_subprocess_exec(
-            *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
-    stdout, stderr = await process.communicate()
-    er = stderr.decode().strip()
-    es = stdout.decode().strip()
-    if es:
-        print(es)
+    os.system("git clone https://github.com/Sh1vam/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install -r requirements.txt && python3 -m userbot")
 except Exception as e:
     print(str(e))
