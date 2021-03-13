9aff534f6fd56325859e155df86a04489ba0d2a7
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2021-03-13 10:36:45 +0530

    Add files via upload

--- sh1vamrun.py
+++ sh1vamrun.py
@@ -1,5 +1,2 @@
 import os
-try:
-    os.system("git clone https://github.com/Sh1vam/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install -r requirements.txt && python3 -m userbot")
-except Exception as e:
-    print(str(e))
+os.system("git clone https://github.com/Sh1vam/DARKCOBRA /root/userbot && cd /root/userbot && pip3 install -r requirements.txt && python3 -m userbot")
